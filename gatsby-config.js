41789e81df8e118cc5015f3355e304f76757be5b
Ismail Gunsaya <ismail.gunsaya@skyscanner.net>
2020-01-17 15:32:16 +0000

    Use only one font

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,8 +38,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `Montserrat`,
-          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+          `Montserrat`
         ],
         display: "swap",
       },
